ad45db4cc134f3a2c1ed9c59f0b64ff02c9be4f5
samir0786 <samirsiescom@gmail.com>
2020-03-26 21:26:11 +0300

    MODIFIED: Storing Daily Wage Using Arrays

--- ProgramminConstruct/Arrays_Problems/empWageStoreDailyWage.sh
+++ ProgramminConstruct/Arrays_Problems/empWageStoreDailyWage.sh
@@ -1,6 +1,41 @@
 #!/bin/bash -x
-counter=0
-Fruits[((counter++))]="Apple"
-Fruits[((counter++))]="Banana"
-Fruits[((counter++))]="orange"
-echo ${Fruits[@]}
+
+# CONSTANTS
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+# VARIABLSE
+totalWorkHours=0;
+totalWorkingDays=0;
+
+function getWorkingHours() {
+	case $1 in
+		$IS_FULL_TIME)
+			workHours=8
+			;;
+		$IS_PART_TIME)
+			workHours=4
+			;;
+		*)
+			workHours=0
+			;;
+	esac
+	echo $workHours
+}
+function calcDailyWage() {
+	local workHrs=$1
+	wage=$(($workHrs*$EMP_RATE_PER_HR))
+	echo $wage
+}
+while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+	((totalWorkingDays++))
+	 workHours="$( getWorkingHours $((RANDOM%3)) )"
+	totalWorkHours=$(($totalWorkHours+$workHours))
+	empDailyWage[$totalWorkingDays]="$( calcDailyWage $workHours )"
+done
+totalSalary="$( calcDailyWage $totalWorkHours )"
+echo "Daily Wage " ${empDailyWage[@]}
